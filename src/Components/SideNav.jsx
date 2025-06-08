35210e3097b38d3705e2bbf97432ea69a6d3fa09
[Muhammed] <[nameismilo2@gmail.com]>
2025-06-08 17:11:35 +0300

    handle on close in responsive nav

--- src/Components/SideNav.jsx
+++ src/Components/SideNav.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import { useState } from 'react'
 import { AiFillMessage } from 'react-icons/ai';
 import { FaBars, FaUser } from 'react-icons/fa';
 import { FaBarsStaggered, FaPhotoFilm } from 'react-icons/fa6';
@@ -7,35 +7,33 @@ import { MdHome } from 'react-icons/md';
 import { Link } from 'react-router-dom';
 
 const SideNav = () => {
-    const Links = [
-        { id: 1, icon: <MdHome />, page: "Home", path: "/" },
-        { id: 2, icon: <FaUser />, page: "About Me", path: "/about" },
-        { id: 4, icon: <FaBarsStaggered />, page: "C.V", path: "/my-cv" },        
-        { id: 3, icon: <AiFillMessage />, page: "Contact Me", path: "/contact" },
-      ];
-    
-      const [selectIndex, setSelectIndex] = useState(0);
-      const [isOpen, setIsOpen] = useState(false);
+  const [selectIndex, setSelectIndex] = useState(0);
+  const [isOpen, setIsOpen] = useState(false);
 
-      
-    
-      return (
-        <>
-        <div className= {isOpen? 'openSideNav' : 'sideNav'}>
-          {Links.length === 0 && <p>Nav not found</p>}
-          <ul className={isOpen ? "show" : "hide"}>
-              {Links.map((link, index) => 
-              <li key={link.id}><Link className={selectIndex === index ? "active" : ""} onClick={() => setSelectIndex(index)} to={link.path} ><p>{link.icon}</p><p>{link.page}</p> </Link> <hr /></li>
-              )}
-          </ul>
-        </div> 
-        <div className='bars'>
-          <p className='barIcons'>
-            {!isOpen ? (<FaBars onClick={() => setIsOpen(true)} />) : (<IoMdClose onClick={() => setIsOpen(false)}/>)}
-          </p>
-        </div>
-        </>
-      )
-    }
+  const Links = [
+      { id: 1, icon: <MdHome />, page: "Home", path: "/" },
+      { id: 2, icon: <FaUser />, page: "About Me", path: "/about" },
+      { id: 4, icon: <FaBarsStaggered />, page: "C.V", path: "/my-cv" },        
+      { id: 3, icon: <AiFillMessage />, page: "Contact Me", path: "/contact" },
+    ];
+  
+    return (
+    <>
+    <div onClick={() => setIsOpen(false)} className= {isOpen? 'openSideNav' : 'sideNav'}>
+      {Links.length === 0 && <p>Nav not found</p>}
+      <ul className={isOpen ? "show" : "hide"}>
+          {Links.map((link, index) => 
+          <li key={link.id}><Link className={selectIndex === index ? "active" : ""} onClick={() => setSelectIndex(index)} to={link.path} ><p>{link.icon}</p><p>{link.page}</p> </Link> <hr /></li>
+          )}
+      </ul>
+    </div> 
+    <div className='bars'>
+      <p className='barIcons'>
+        {!isOpen ? (<FaBars onClick={() => setIsOpen(true)} />) : (<IoMdClose onClick={() => setIsOpen(false)}/>)}
+      </p>
+    </div>
+    </>
+    )
+  }
 
 export default SideNav
